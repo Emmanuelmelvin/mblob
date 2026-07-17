17ab6994d2a4b0ed9aadc9db69caed75371a1d4a
Emma Chid <88337364+Emmanuelmelvin@users.noreply.github.com>
2026-07-17 01:40:46 +0100

    Switch frontend API port to 5000

--- check_storage_nodes.sh
+++ check_storage_nodes.sh
@@ -9,4 +9,4 @@ docker exec mblob-storage-node-2-1 ls -l /data
 docker exec mblob-storage-node-3-1 ls -l /data
 
 # Check if storage-node is listed as available from gateway
-docker exec mblob-gateway-1 curl -s http://localhost:3000/api/health/storage-nodes | jq ."
+docker exec mblob-gateway-1 curl -s http://localhost:5000/api/health/storage-nodes | jq ."
