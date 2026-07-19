e898cc028c65260e239d62fb13b820ff6ccd9e48
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2026-07-18 19:05:51 -0700

    edited registry address.

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const CONTRACT = {
     RPC_URL: 'https://testnet-rpc.monad.xyz',
-    REGISTRY_ADDRESS: '0xd752eb79D2b12436A319dEeeF69407425cbe54a9' as `0x${string}`,
+    REGISTRY_ADDRESS: '0xE01CB95C7f62831389b288b87770238D75C6C3ef' as `0x${string}`,
     CHAIN_ID: 10_143,
     EXPLORER_URL: 'https://testnet.monadexplorer.com',
 } as const
