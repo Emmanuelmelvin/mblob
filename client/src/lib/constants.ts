ef192719e5fe2e6c897dd2e9029ed0d0dc298265
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2026-07-18 18:49:16 -0700

    changed contract address.

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const CONTRACT = {
     RPC_URL: 'https://testnet-rpc.monad.xyz',
-    REGISTRY_ADDRESS: '0xBe67Af35e0E46443F0c85209497a780143CE9e4B' as `0x${string}`,
+    REGISTRY_ADDRESS: '0xd752eb79D2b12436A319dEeeF69407425cbe54a9' as `0x${string}`,
     CHAIN_ID: 10_143,
     EXPLORER_URL: 'https://testnet.monadexplorer.com',
 } as const
